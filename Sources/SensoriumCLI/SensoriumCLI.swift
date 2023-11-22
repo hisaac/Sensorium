8797500e6ada78c4f03a32a4a2d607c4493f72cb
Isaac Halvorson <hello@hisaac.net>
2023-11-22 06:00:39 -0600

    Create SysInfoKit
    Signed-off-by: Isaac Halvorson <hello@hisaac.net>

--- Sources/SensoriumCLI/SensoriumCLI.swift
+++ Sources/SensoriumCLI/SensoriumCLI.swift
@@ -1,9 +1,12 @@
 import Foundation
 import SensoriumKit
+import SysInfoKit
 
 @main
 struct SensoriumCLI {
 	static func main() throws {
+		print(SysInfoKit.systemInfo())
+		print()
 		let switchBoard = SwitchBoard()
 		switchBoard.start(debug: true)
 		RunLoop.main.run()
---
+++ Sources/SysInfoKit/SysInfoKit.swift
@@ -0,0 +1,14 @@
+//
+//  SysInfoKit.swift
+//  SysInfoKit
+//
+//  Created by Isaac Halvorson on 11/22/23.
+//
+
+import Foundation
+
+public enum SysInfoKit {
+	public static func systemInfo() -> String {
+		return "systemInfo coming soon!"
+	}
+}
